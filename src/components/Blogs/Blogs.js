6f0e2a3be504165b11351d2519a77ae27ef4ba43
LabibIslam <labib.islam.practice@gmail.com>
2022-04-04 14:49:57 +0600

    added some besic route for all pages

---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h3>hello from Blogs</h3>
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ src/components/Review/Review.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Review = () => {
+    return (
+        <div>
+            <h4>hello from review</h4>
+        </div>
+    );
+};
+
+export default Review;
